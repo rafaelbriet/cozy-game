e33040dafc111dc5018648a779cba326e2b0a4cf
Rafael Briet <rafael-briet@hotmail.com>
2023-01-19 14:59:25 -0300

    Create inventory UI

---
+++ Assets/Scripts/Runtime/Inventory.cs
@@ -0,0 +1,11 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace CozyGame
+{
+    public class Inventory : MonoBehaviour
+    {
+        [SerializeField]
+        private List<Item> _items;
+    }
+}
