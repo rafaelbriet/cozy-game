876071b5776b8003cdff949ed1c04551ad09d03c
Rafael Briet <rafael-briet@hotmail.com>
2023-01-19 23:19:04 -0300

    Shopping window display prices

--- Assets/Scripts/Runtime/Shop/ShopShopkeeperInventorySlot.cs
+++ Assets/Scripts/Runtime/Shop/ShopShopkeeperInventorySlot.cs
@@ -10,11 +10,14 @@ namespace CozyGame
         private Image _itemIcon;
         [SerializeField]
         private TextMeshProUGUI _itemName;
+        [SerializeField]
+        private TextMeshProUGUI _itemPrice;
 
         public void Init(Item item)
         {
             _itemIcon.sprite = item.Icon;
             _itemName.SetText(item.ItemName);
+            _itemPrice.SetText(item.Price.ToString());
         }
     }
 }
